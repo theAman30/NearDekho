e40746166c2b1cbf5a08cfac9cbc345e68913387
theAman30 <a.srivastav1005@gmail.com>
2024-09-20 20:09:03 +0530

    added scroll to top button for better performance of the web page.

--- js/main.js
+++ js/main.js
@@ -8,3 +8,30 @@ function Menu(e) {
         list.classList.remove("top-[80px]"),
         list.classList.remove("opacity-100"));
   }
+
+// javascript function to make scroll to top functionality and animation hover remove and add functionality
+const scrollTopBtn = document.getElementById("scrollTopBtn");
+const icon = scrollTopBtn.querySelector("i");
+
+// Disable animation on hover
+scrollTopBtn.addEventListener("mouseover", () => {
+  icon.classList.remove("fa-bounce");
+});
+
+// Re-enable animation after hover
+scrollTopBtn.addEventListener("mouseout", () => {
+  icon.classList.add("fa-bounce");
+});
+
+// Show the button when the user scrolls down 400px from the top of the page
+window.onscroll = function () {
+  if (document.body.scrollTop > 400 || document.documentElement.scrollTop > 400) {
+    scrollTopBtn.classList.remove("hidden");
+  } else {
+    scrollTopBtn.classList.add("hidden");
+  }
+};
+
+scrollTopBtn.onclick = function () {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
+};
