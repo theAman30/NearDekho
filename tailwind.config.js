94812ca2fca453027989536aa7a950225993e464
theAman30 <a.srivastav1005@gmail.com>
2024-10-03 13:40:21 +0530

    add a new html file to create all the blogs.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["*.html, ./js/main.js"],
+  content: ["*.html, ./js/main.js, ./blogs/*.html"],
   theme: {
     extend: {},
   },
